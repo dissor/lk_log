96fa57422f05b93c6d9b842324d00c6be1d6df8b
Jonas <guojianwenjonas@foxmail.com>
2024-10-08 16:13:54 +0800

    备份

--- lk_log.h
+++ lk_log.h
@@ -8,16 +8,16 @@
  * @brief 自定义配置
  *
  */
-#ifndef LOG_LEVEL
-#define LOG_LEVEL LOG_VERBOSE // log等级，0关闭所有log
+#ifndef LK_LOG_LEVEL
+#define LK_LOG_LEVEL LK_LOG_VERBOSE // log等级，0关闭所有log
 #endif
 
-#define LOG_TIME_STAMP (rt_tick_get_millisecond()) // 时间戳
+#define LK_LOG_TIME_STAMP (rt_tick_get_millisecond()) // 时间戳
 
-#define LOG_FILE __FILE__
-// #define LOG_FUNC __FUNCTION__
-#define LOG_LINE __LINE__
-#define LOG_END  "\r\n"
+#define LK_LOG_FILE __FILE__
+// #define LK_LOG_FUNC __FUNCTION__
+#define LK_LOG_LINE __LINE__
+#define LK_LOG_END  "\r\n"
 
 #define dump_print     printf
 #define __is_print(ch) ((unsigned int)((ch) - ' ') < 127u - ' ')
@@ -39,38 +39,38 @@
  *
  */
 
-#define LOG_NONE    0 // 关闭
-#define LOG_ERROR   1 // 错误
-#define LOG_WRANING 2 // 警告
-#define LOG_INFO    3 // 消息
-#define LOG_DEBUG   4 // 调试
-#define LOG_VERBOSE 5 // 冗余
+#define LK_LOG_NONE    0 // 关闭
+#define LK_LOG_ERROR   1 // 错误
+#define LK_LOG_WRANING 2 // 警告
+#define LK_LOG_INFO    3 // 消息
+#define LK_LOG_DEBUG   4 // 调试
+#define LK_LOG_VERBOSE 5 // 冗余
 
 /**
  * @brief Log打印接口
  *
  */
-#define logFmt(text, level, fmt, ...)                        \
-    if (level <= LOG_LEVEL) {                                \
-        printf(text " " fmt "" LOG_END,                      \
-               LOG_TIME_STAMP / 1000, LOG_TIME_STAMP % 1000, \
-               LOG_FILE, LOG_LINE, ##__VA_ARGS__);           \
+#define logFmt(text, level, fmt, ...)                              \
+    if (level <= LK_LOG_LEVEL) {                                   \
+        printf(text " " fmt "" LK_LOG_END,                         \
+               LK_LOG_TIME_STAMP / 1000, LK_LOG_TIME_STAMP % 1000, \
+               LK_LOG_FILE, LK_LOG_LINE, ##__VA_ARGS__);           \
     }
 
-#define LOG_E(fmt, ...) \
-    logFmt(ERROR_TEXT, LOG_ERROR, fmt, ##__VA_ARGS__)
+#define LK_LOG_E(fmt, ...) \
+    logFmt(ERROR_TEXT, LK_LOG_ERROR, fmt, ##__VA_ARGS__)
 
-#define LOG_W(fmt, ...) \
-    logFmt(WARNING_TEXT, LOG_WRANING, fmt, ##__VA_ARGS__)
+#define LK_LOG_W(fmt, ...) \
+    logFmt(WARNING_TEXT, LK_LOG_WRANING, fmt, ##__VA_ARGS__)
 
-#define LOG_I(fmt, ...) \
-    logFmt(INFO_TEXT, LOG_INFO, fmt, ##__VA_ARGS__)
+#define LK_LOG_I(fmt, ...) \
+    logFmt(INFO_TEXT, LK_LOG_INFO, fmt, ##__VA_ARGS__)
 
-#define LOG_D(fmt, ...) \
-    logFmt(DEBUG_TEXT, LOG_DEBUG, fmt, ##__VA_ARGS__)
+#define LK_LOG_D(fmt, ...) \
+    logFmt(DEBUG_TEXT, LK_LOG_DEBUG, fmt, ##__VA_ARGS__)
 
-#define LOG_V(fmt, ...) \
-    logFmt(VERBOSE_TEXT, LOG_VERBOSE, fmt, ##__VA_ARGS__)
+#define LK_LOG_V(fmt, ...) \
+    logFmt(VERBOSE_TEXT, LK_LOG_VERBOSE, fmt, ##__VA_ARGS__)
 
 /**
  * @brief 16进制打印数据
@@ -79,7 +79,8 @@
  * @param size 数据区大小
  * @param number 每行输出数据的数量
  */
-static void LOG_HEX(const char *buf, uint32_t size, uint32_t number) {
+#if 0
+static void LK_LOG_HEX(const char *buf, uint32_t size, uint32_t number) {
     for (uint32_t i = 0; i < size; i += number) {
         dump_print("%08X: ", i);
 
@@ -105,5 +106,13 @@ static void LOG_HEX(const char *buf, uint32_t size, uint32_t number) {
         dump_print("\r\n");
     }
 }
+#endif
+
+// ASS
+#define LOG_E LK_LOG_E
+#define LOG_W LK_LOG_W
+#define LOG_I LK_LOG_I
+#define LOG_D LK_LOG_D
+#define LOG_V LK_LOG_V
 
 #endif /* __LK_LOG_H__ */
